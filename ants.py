6da1798273eea8f88619dfe4320c3b8360e81cbc
Derek Adam <derekadam1988@gmail.com>
2015-09-05 19:14:01 -0500

    Added more attract fields

--- ants.py
+++ ants.py
@@ -10,16 +10,21 @@ white = (255,255,255)
 size = [400, 400]
 screen = pygame.display.set_mode(size)
  
-pygame.display.set_caption("Example code for the draw module")
+pygame.display.set_caption("Ants Sim")
  
 # Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 
 ants = []
+
 for i in range(1000):
-    ants.append([random.randrange(195,205), random.randrange(195, 205   )])
-    ants[i] = [200,200]
+    ants.append([random.randrange(0,400), random.randrange(0, 400   )])
+    # ants[i] = [200,200]
+
+balls = []
+for i in range(10):
+    balls.append([random.randrange(0,400), random.randrange(0,400)])
 
 def move_ant(ant, direction):
     dic = {"left": (-1,0), "right": (1,0), "up": (0,-1), "down": (0,1)}
@@ -28,23 +33,29 @@ def move_ant(ant, direction):
     ant[0] += rmove[0]
     ant[1] += rmove[1]
 
-def movesmart(ant):
-    for newmove in smartmoves:
-        if get_distance((newmove[0] + ant[0], newmove[1] + ant[1]),(200,225)) < get_distance(ant, (200,225)):
-            return newmove
-
-def moverepel(ant):
-    for newmove in smartmoves:
-        if get_distance((newmove[0] + ant[0], newmove[1] + ant[1]),(180,180)) > get_distance(ant, (180,180)):
-            return newmove
-
-smartmoves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-
 def get_distance(origin, destination):
     x = origin[0] - destination[0]
     y = origin[1] - destination[1]
     return math.sqrt(x*x + y*y)
+
+def attract(ant, attract_pos):
+    movelist = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    possible_move = []
+    # check if up/down/right/left will lead them closer to orb
+    # append to list, and pick at random
+    for move in movelist:
+        if get_distance((move[0] + ant[0], move[1] + ant[1]),(attract_pos[0], attract_pos[1])) < get_distance(ant, (attract_pos[0],attract_pos[1])):
+            possible_move.append(move)
+        else:
+            pass
+    k = random.choice(possible_move)
+    if len(k) > 0:
+        ant[0] += k[0]
+        ant[1] += k[1]
+    else:
+        pass
+
+movelist = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 while not done:
  
@@ -56,46 +67,68 @@ while not done:
     # inside the main while done==False loop.
  
     # Clear the screen and set the screen background
-    screen.fill(white)
- 
-    # Draw on the screen a green line from (0,0) to (50.75)
-    # 5 pixels wide.
-
-    pygame.draw.circle(screen, [255,0,0], [200,225], 1)
-    pygame.draw.circle(screen, [255,0,0], [180,180], 1)
+    screen.fill(black)
+    # attract
+    # pygame.draw.circle(screen, [255,0,0], [200,225], 1)
     
-    # Draw a circle
+    # repel
+    pygame.draw.circle(screen, [255,0,0], [180,180], 1)
+
+
+    # counts = dict()
+    # for ant in ants:
+    #     counts[(ant[0], ant[1])] = counts.get((ant[0], ant[1]), 0) + 1
+
+  
     for ant in ants:
-        pygame.draw.circle(screen, black, ant, 0)
- 
+        # screen.set_at(ant, 255)
+        pygame.draw.circle(screen, [0,255,0], ant, 0)
     # Go ahead and update the screen with what we've drawn.
     # This MUST happen after all the other drawing commands.
     pygame.display.flip()
 
     for ant in ants:
-        if get_distance(ant,(200,225)) < 10:
-            k = movesmart(ant)
-            ant[0] += k[0]
-            ant[1] += k[1]
+        # if ant is less than 10 pixels away from attracting orb...
+        # if get_distance(ant, (200, 225)) < 10:
+        #     attract(ant, (200,225))
+
+        # Same but for repellings
+        if get_distance(ant, (180, 180)) < 50:
+            possible_move = []
             
-        elif get_distance(ant, (180, 180)) < 10:
-            k = moverepel(ant)
+            for move in movelist:
+                if get_distance((move[0] + ant[0], move[1] + ant[1]),(180,180)) > get_distance(ant, (180,180)):
+                    possible_move.append(move)
+
+
+            k = random.choice(possible_move)
             ant[0] += k[0]
             ant[1] += k[1]
 
+        for ball in balls:
+            if get_distance(ant, ball) < 50 and get_distance(ant, ball) > 0:
+                attract(ant, ball)
+        # elif get_distance(ant, balls[0]) < 50:
+        #     attract(ant, balls[0])
+
         else:
-			moves = ["left", "up", "down", "right"]
-			move_ant(ant, random.choice(moves))
+            #Normal random move sets
+            moves = ["left", "up", "down", "right"]
+            move_ant(ant, random.choice(moves))
 
 
     for ant in ants[:]:
         # if get_distance(ant, (210,210)) < 5:
         #     ant[0] = 210
         #     ant[1] = 210
-        if ant == [200, 225]:
-            ants.remove(ant)
-        
+        # if ant == [200, 225]:
+        #     ants.remove(ant)
 
+        for ball in balls:
+            if ant == ball:
+                ants.remove(ant)
+        
+    print len(ants)
 
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.

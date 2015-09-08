1f9d5382fe319837879b20d59e54eb969ad3c7ae
Derek Adam <derekadam1988@gmail.com>
2015-09-08 02:07:34 -0500

    Created Ant class, refactored loop for ant movement

--- ants.py
+++ ants.py
@@ -1,64 +1,67 @@
 '''
-Ant colony simulator or black holes and stars
+Ant colony simulator or BLACK holes and stars
 '''
 import pygame, random, math
 
+class Ant():
+
+    moves = {"left": (-1,0), "right": (1,0), "up": (0,-1), "down": (0,1), "upleft": (-1,-1), "downleft": (-1,1), "upright": (1,-1), "bottomright": (1,1)}
+
+    def __init__(self):
+        self.pos = [random.randrange(0,400),random.randrange(0,400)]
+
+
+    def move_ant(self, new_position):
+        self.pos[0] = new_position[0]
+        self.pos[1] = new_position[1]
+
+    def add_xys(self, xy):
+        self.pos[0] += xy[0]
+        self.pos[1] += xy[1]
+
+    def get_closer(self, move, new_position):
+        current_delta = get_distance(self.pos, new_position)
+        self.add_xys(move)
+        delta = get_distance(self.pos, new_position)
+        return delta < current_delta
+
+
+
+def get_distance(origin, destination):
+    '''(x,y), (x,y) -> returns float
+    Calculates distance from (x,y) to (x,y)
+    '''
+    x = origin[0] - destination[0]
+    y = origin[1] - destination[1]
+    return math.sqrt(x*x + y*y)
+
+def within_distance(origin, destination, distance):
+    '''(x,y) , (x,y), int -> returns bool
+    Checks if origin to destination is within a distance
+    '''
+    return get_distance(origin, destination) <= distance
 
 def main(): 
     # Initialize the game engine
     pygame.init()
      
     # Define the colors we will use in RGB format
-    black = (0, 0, 0)
-    white = (255,255,255)
-    # Set the height and width of the screen
-    size = [400, 400]
-    screen = pygame.display.set_mode(size)
+    BLACK = (0, 0, 0)
+    WHITE = (255,255,255)
+    # Set the height and width of the SCREEN
+    SIZE = [400, 400]
+    SCREEN = pygame.display.set_mode(SIZE)
      
     pygame.display.set_caption("Ants Sim")
     clock = pygame.time.Clock() 
 
     # Create list of ants
     ants = []   
-    for i in range(1000):
-        ants.append([random.randrange(0,400), random.randrange(0, 400   )])
-
+    for i in range(25):
+        ants.append(Ant())
+        
     # Create list of balls (attracting balls)
     balls = []
-    
-    def move_ant_to(ant, pos):
-        '''(x,y), (x,y) -> None
-
-        Modifies ant position by adding pos to it.
-        '''
-        ant[0] += pos[0]
-        ant[1] += pos[1]
-
-    def get_distance(origin, destination):
-        '''(x,y), (x,y) -> returns float
-        Calculates distance from (x,y) to (x,y)
-        '''
-        x = origin[0] - destination[0]
-        y = origin[1] - destination[1]
-        return math.sqrt(x*x + y*y)
-
-
-    def legit_moves(ant, attract_pos):
-        '''
-        (x,y), (x,y) -> list of possible moves
-
-        Generates a list of possible moves that takes the intial x,y closer to the second x,y
-        '''
-        movelist = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]]
-        possible_moves = list()
-        for move in movelist:
-
-            if get_distance((move[0] + ant[0], move[1] + ant[1]),(attract_pos[0], attract_pos[1])) < get_distance(ant, (attract_pos[0],attract_pos[1])):
-                possible_moves.append(move)
-            else:
-                pass
-
-        return possible_moves
 
     queue = 0
     # Loop until the user clicks the close button.
@@ -71,7 +74,7 @@ def main():
                 if len(balls) < 3:
                     balls.append(pos)
                 else:
-                    # Queue which only puts 3 balls on screen at once
+                    # Queue which only puts 3 balls on SCREEN at once
                     balls.remove(balls[queue])
                     balls.insert(queue, pos)
                     queue += 1
@@ -88,44 +91,27 @@ def main():
             # If in proximity of 50 pixels, attract towards ball
             for ball in balls:
                 
-                if get_distance(ant, ball) < 50 and get_distance(ant, ball) >= 0:
-                    attracting_moves = legit_moves(ant, ball)
+                if within_distance(ant.pos, ball, 50):
+                    possible_moves = []
+                    # If within range of attracting orb...
+                    for move in ant.moves.keys():
+                        if ant.get_closer(ant.moves[move], ball):
+                            possible_moves.append(ant.moves[move])
 
-                    # If there is moves left to attract to ball...
-                    if attracting_moves:
-                        move_ant_to(ant, random.choice(attracting_moves))
+                    ant.add_xys(random.choice(possible_moves))
 
-
-                else:
-                    # Choose random direction and move
-                    dic = {"left": (-1,0), "right": (1,0), "up": (0,-1), "down": (0,1), "upleft": (-1,-1), "downleft": (-1,1), "upright": (1,-1), "bottomright": (1,1)}
-                    moves = ["left", "up", "down", "right"]
-                    random_pick = random.choice(moves)
-                    move_ant_to(ant, dic[random_pick])
-        '''
-        # Set kill = True to remove ants from game when sucked into balls
-        # Can't remove from iterating list so we make a copy using ants[:]
-        kill = False
-        if kill:
-            for ant in ants[:]:
-                # If ant is at same coordinate as ball, remove ant from screen
-                for ball in balls:
-
-                    if ant[0] == ball[0] and ant[1] == ball[1]:
-                        ants.remove(ant)
-        '''
         # Draw ants
         for ant in ants:
-            pygame.draw.rect(screen, [0,255,0], [ant[0], ant[1],1,1], 1)
+            pygame.draw.rect(SCREEN, [0,255,0], [ant.pos[0], ant.pos[1],1,1], 1)
 
         for ball in balls:
-            pygame.draw.circle(screen, [255,0,0], ball, 2)
+            pygame.draw.circle(SCREEN, [255,0,0], ball, 2)
 
         pygame.display.flip()
-        # Clear the screen and set the screen background
-        screen.fill(black)
+        # Clear the SCREEN and set the SCREEN background
+        SCREEN.fill(BLACK)
 
-        clock.tick()
+        clock.tick(60)
      
     # Can't use X on window withou this command
     pygame.quit()
